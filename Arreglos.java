b4ae1222235fdb1dfe543d44a6e1ca4b57606b74
ivan <scorpoin32@gmail.com>
2021-05-22 19:35:17 -0500

    se agregó arrelgos

---
+++ Arreglos.java
@@ -0,0 +1,17 @@
+public class Arreglis{
+	public static void main(String args[]){
+
+		int arreglo[] = new int [5];
+
+		arreglo[0] = 4;
+		arreglo[1] = 34;
+		arreglo[2] = 14;
+		arreglo[3] = 347;
+		arreglo[4] = -56;
+
+		for(int i=0; i<arrreglo.length ; i++){
+			System.out.println(arreglo[i]);
+		}
+
+	}
+}
