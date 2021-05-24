00c50b6e89e1bb12f7788371068c9e5fd473cae5
ivan <scorpoin32@gmail.com>
2021-05-23 20:56:47 -0500

    variables de arreglos

---
+++ Erreg.java
@@ -0,0 +1,18 @@
+public class Erreg{
+	public static void main(String args[]){
+
+	int arreglo[] = new int [5];
+
+	arreglo[0] = 8;
+	arreglo[1] = 16;
+	arreglo[2] = 26;
+	arreglo[3] = 108;
+	arreglo[4] = -678;
+
+	//una forma resumida de hacer el arreglo.
+
+	for(int i:arreglo){
+	   System.out.println(i);
+	}
+	}
+}
