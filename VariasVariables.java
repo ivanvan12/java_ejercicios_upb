5d2d2832cc59689bf15a725b35334572ef56afbe
ivan <scorpoin32@gmail.com>
2021-06-09 16:22:18 -0500

    se añade el programa varias variables que concatenan archivos de un mismo tipo separado por comas

---
+++ VariasVariables.java
@@ -0,0 +1,13 @@
+public class VariasVariables {
+	public static void main(String args[]) {
+    //se pueden concatenar varias variables de un mismo tipo, por ejemplo tipo entero (int) separadas por coma una vez se hayan inicializado.
+	int a=12,b=45,c=33;
+	String hello="hola mundo",pedro="argon";
+
+	System.out.println(hello);
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(pedro);
+	}
+}
