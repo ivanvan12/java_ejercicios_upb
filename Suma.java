59f965d2b52976c7d98f5ed4b57688677b82f126
ivan <scorpoin32@gmail.com>
2021-06-09 17:29:12 -0500

    suma

---
+++ Suma.java
@@ -0,0 +1,11 @@
+public class Suma{
+	public static void main(String args[]){
+		// resta,suma,multiplicacion, division y operador modulo (%).
+		int x = 7 + 10 * 3 -40,y = 45 % 3 /2;
+
+		int result = x + y;
+
+		System.out.println(result);//crear una variable tipo entero y convovarla para que almacene el resultado de la suma de x y y.
+		System.out.println(x+y);//otra manera de sumar 
+	}
+}
