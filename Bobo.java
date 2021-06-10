79b24f7bad9ddc578b4c6472c84ea444bbe47ac2
ivan <scorpoin32@gmail.com>
2021-06-09 22:44:55 -0500

    se agrega el programa Bobo que contiene el sufijo de x

---
+++ Bobo.java
@@ -0,0 +1,8 @@
+public class Bobo{
+	public static void main(String[] args) {
+		int x = 2;
+		//primero se asigna x a la impresion(x++) y luego al imprimir x se le suma a la variable el ++.
+		System.out.println(x++);
+		System.out.println(x);
+	}
+}
