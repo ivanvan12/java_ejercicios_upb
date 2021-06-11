3f2d79371e6969a97a4bf96e91fac6616faf266f
ivan <scorpoin32@gmail.com>
2021-06-11 14:42:49 -0500

    Se agregan los programas de cadenas de texto ,conca y str

---
+++ Conca.java
@@ -0,0 +1,8 @@
+public class Conca {
+	public static void main(String[] args) {
+		String nombre ="Ivan ",apellido ="Zuñiga";
+
+		System.out.println("Mi nombre es "+nombre+apellido);
+		System.out.println("pa que tanta joda si se puede imprimir lo mismo aquí :v");
+	}
+}
---
+++ Str.java
@@ -0,0 +1,7 @@
+public class Str {
+	public static void main(String[] args) {
+		//Se pueden crear String's vacios : . ej:.
+		String a = "";
+		System.out.println(a);
+	}
+}
